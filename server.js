5a411fa2fbfac333d7ed76e4d0c32de17cff30df
Jonathan Deleon <Jondeleon856@gmail.com>
2020-06-01 14:26:12 -0400

    connect to DB

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require('express');
+const connectDB = require('./config/db');
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
 const app = express();
 
+connectDB();
 const port = 8000;
 
+app.use(express.json({extended:false}));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/api/userModel', require('./api/User'));
 require('./app/routes')(app, {});
 app.listen(port, () => {  
     console.log('We are live on ' + port);
 });
+
+// QnLcUqQXxLsXa4Rx
