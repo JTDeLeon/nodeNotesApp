1dfdaf60eb27201d0892e77862b0b8d379690780
Jonathan Deleon <Jondeleon856@gmail.com>
2020-06-03 17:57:16 -0400

    update notes api to handle delete and update of notes

--- README.md
+++ README.md
@@ -5,6 +5,26 @@
 - Mongoose
 - MongoDB
 
+# How to Use API: 
+Make a Note: [POST Request]
+http://localhost:8000/api/noteModel
+Put an ID from the database to show the note
+Be sure to use body parameter ( title , bodyText)
+
+Read a Note: [GET Request]
+http://localhost:8000/note/:id 
+Put an ID from the database to show the note
+
+Delete a Note: [DELETE Request]
+http://localhost:8000/note/:id 
+Put an ID from the database to show the note
+
+Update a Note: [PUT Request]
+http://localhost:8000/note/:id 
+Put an ID from the database to show the note
+Be sure to use body parameter ( title , bodyText) * Both are required
+
+
 ## https://www.freecodecamp.org/news/building-a-simple-node-js-api-in-under-30-minutes-a07ea9e390d2/
 
 ## https://cloud.mongodb.com/
--- api/Note.js
+++ api/Note.js
@@ -33,4 +33,45 @@ route.get('/', function(req, res, next) {
     // res.send('This is where I can retrieve a /note');
 });
 
+route.delete('/', function(req, res, next) {
+    console.log(req.params.id)
+    const message = Note
+    .findByIdAndDelete(req.params.id)
+        .then(() => {
+           console.log('note deleted');
+        //    res.send('note deleted');
+        })
+        .catch(err=>next(err))
+
+    res.json({message})
+            
+
+    // res.send('This is where I can retrieve a /note');
+});
+
+route.put('/', function(req, res, next) {
+    console.log(req.params.id)
+
+    const{title,bodyText} = req.body;
+    let note = {};
+    note.title = title;
+    note.bodyText = bodyText;
+
+    Note.findByIdAndUpdate(req.params.id, {
+        title : note.title,
+        bodyText : note.bodyText
+    }, 
+    (err, result) =>{
+        if (err )
+        res.send(err);
+        else{
+        res.send(result)
+        console.log('updated note ', req.params.id)
+        }
+    })
+            
+
+    // res.send('This is where I can retrieve a /note');
+});
+
 module.exports = route;
